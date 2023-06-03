29fe3e42313570f552f33972099fe182cbeaa228
tokyosuperch <tokyo_superch@yahoo.co.jp>
2023-06-03 19:55:40 +0900

    Changed to pre-pagenated style

--- images2epub.sh
+++ images2epub.sh
@@ -6,11 +6,27 @@ cp -r template/ebook temporary/$title
 rm temporary/$title/ebook/item/xhtml/page.xhtml
 rm temporary/$title/ebook/item/images/.gitkeep
 standard=$(cat temporary/$title/ebook/item/standard.opf)
-page=$(cat template/ebook/item/xhtml/page.xhtml)
+pagetemp=$(cat template/ebook/item/xhtml/page.xhtml)
 standard=$(echo "$standard" | sed -e "s/<!--title-->/$title/g" -e "s/<!--creator-->/$author/g")
 for pathfile in `ls ./input`; do
     cp ./input/$pathfile temporary/$title/ebook/item/images/
-    echo "$page" | sed -e "s/<!--imagepath-->/..\/images\/$pathfile/g" -e "s/<!--title-->/$title/g" > temporary/$title/ebook/item/xhtml/$pathfile.xhtml
+    page=$(echo "$pagetemp" | sed \
+    -e "s/<!--imagepath-->/..\/images\/$pathfile/g" \
+    -e "s/<!--title-->/$title/g")
+    fileinfo=$(file temporary/$title/ebook/item/images/$pathfile)
+    filetype=$(echo $fileinfo | awk -F'[:,]' '{print $2}')
+    if [ "$filetype" = " JPEG image data" ]; then
+        resolution=$(echo $fileinfo | awk -F'[:,]' '{print $9}')
+        width=$(echo $resolution | awk -F'[ x]' '{print $1}')
+        height=$(echo $resolution | awk -F'[ x]' '{print $2}')
+        page=$(echo "$page" | sed -e "s/<!--viewport-->/<meta name=\"viewport\" content=\"width=$width, height=$height\" \/>/g")
+    elif [ "$filetype" = " PNG image data" ]; then
+        resolution=$(echo $fileinfo | awk -F'[:,]' '{print $3}')
+        width=$(echo $resolution | awk -F'[ x]' '{print $1}')
+        height=$(echo $resolution | awk -F'[ x]' '{print $4}')
+        page=$(echo "$page" | sed -e "s/<!--viewport-->/<meta name=\"viewport\" content=\"width=$width, height=$height\" \/>/g")
+    fi
+    echo "$page" > temporary/$title/ebook/item/xhtml/$pathfile.xhtml
     standard=$(echo "$standard" | sed \
     -e "s/<!--xhtml+xml-->/<item media-type=\"application\/xhtml+xml\" id=\"$pathfile\" href=\"xhtml\/$pathfile.xhtml\"\/>\n<!--xhtml+xml-->/g" \
     -e "s/<!--itemref-->/<itemref idref=\"$pathfile\" \/>\n<!--itemref-->/g")
