f2b13e378d7a2e3e2033f655d60b73c39b021fbb
tokyosuperch <tokyo_superch@yahoo.co.jp>
2023-06-03 17:38:44 +0900

    Improve file I/O

--- images2epub.sh
+++ images2epub.sh
@@ -5,16 +5,17 @@ mkdir temporary/$title
 cp -r template/ebook temporary/$title
 rm temporary/$title/ebook/item/xhtml/page.xhtml
 rm temporary/$title/ebook/item/images/.gitkeep
-sed -i -e "s/<!--title-->/$title/g" -e "s/<!--creator-->/$author/g" temporary/$title/ebook/item/standard.opf
+standard=$(cat temporary/$title/ebook/item/standard.opf)
+page=$(cat template/ebook/item/xhtml/page.xhtml)
+standard=$(echo "$standard" | sed -e "s/<!--title-->/$title/g" -e "s/<!--creator-->/$author/g")
 for pathfile in `ls ./input`; do
     cp ./input/$pathfile temporary/$title/ebook/item/images/
-    cat template/ebook/item/xhtml/page.xhtml | sed -e "s/<!--imagepath-->/..\/images\/$pathfile/g" -e "s/<!--title-->/$title/g" > temporary/$title/ebook/item/xhtml/$pathfile.xhtml
-    sed -i -e "s/<!--title-->/$title/g" temporary/$title/ebook/item/xhtml/$pathfile.xhtml
-    sed -i \
-    -e "s/<!--xhtml+xml-->/<item media-type=\"application\/xhtml+xml\" id=\"$pathfile\" href=\"xhtml\/$pathfile.xhtml\"\/>\r\n<!--xhtml+xml-->/g" \
-    -e "s/<!--itemref-->/<itemref idref=\"$pathfile\" \/>\r\n<!--itemref-->/g" \
-    temporary/$title/ebook/item/standard.opf
+    echo "$page" | sed -e "s/<!--imagepath-->/..\/images\/$pathfile/g" -e "s/<!--title-->/$title/g" > temporary/$title/ebook/item/xhtml/$pathfile.xhtml
+    standard=$(echo "$standard" | sed \
+    -e "s/<!--xhtml+xml-->/<item media-type=\"application\/xhtml+xml\" id=\"$pathfile\" href=\"xhtml\/$pathfile.xhtml\"\/>\n<!--xhtml+xml-->/g" \
+    -e "s/<!--itemref-->/<itemref idref=\"$pathfile\" \/>\n<!--itemref-->/g")
 done
+echo "$standard" > temporary/$title/ebook/item/standard.opf
 cd ./temporary/$title/
 zip -r ../../output/$title.epub ebook
 zip -0 ../../output/$title.epub ebook/mimetype 
